0a9aed2ed4c82944bf2c442b04a70a7d3ec2cc42
Ирина Новикова <irishnovikova@icloud.com>
2026-05-29 23:27:17 +0700

    Initial commit

---
+++ client/HeartBeat/HeartBeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartBeat
+//
+//  Created by Ирина Новикова on 29.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ client/HeartBeat/HeartBeat/HeartBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatApp.swift
+//  HeartBeat
+//
+//  Created by Ирина Новикова on 29.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
